79d4df8a1fe5ab5d1cec447c391738e1ea9ce1af
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-06 22:02:55 +0200

    added solutions for final tasks

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.lang.reflect.Array;
+
 public class NumberReverter {
     public void revert(int number) {
+        int reverse;
+        for(reverse = 0; number != 0; number /= 10) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+        }
+
+        System.out.println("The reverse of the given number is: " + reverse);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+
+        int farenheitTemperature = (temperatureCelsius * 9) / 5 + 32;
+        System.out.println(farenheitTemperature);
+
     }
 }
